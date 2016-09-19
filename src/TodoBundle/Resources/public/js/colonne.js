aacc797f7ac993afb599461dad38af30bde59473
tgamess <tleclere@oolink.fr>
2016-09-19 18:00:23 +0200

    Droppable et draggable en préparations ;)

--- src/TodoBundle/Resources/public/js/colonne.js
+++ src/TodoBundle/Resources/public/js/colonne.js
@@ -1,10 +1,24 @@
 $(document).ready(function(){
 
+    $('select').material_select();
+
     //todo creation of tache
     $('#add-task').click(function(e){
         $('#modal-new-task').openModal();
     });
 
     //todo glisser tache
+    $( ".draggable" ).draggable({
+        revert:function(event) {
+            console.log(event);
+            if(!event.hasClass('yes')){
+                return true;
+            }
+        }
+    });
+    $(".droppable").droppable({
+        hoverClass: "z-depth-1",
+        accept: ".draggable"
+    });
 
 });
