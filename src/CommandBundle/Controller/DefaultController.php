8e90075891a52af056df48e0ca00c3058077a1ba
Thibaut Leclere <t.leclere@ext.vivarte.fr>
2017-06-29 17:17:28 +0200

    Bundle command en WIP nouveaux fichiers

---
+++ src/CommandBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace CommandBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('CommandBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
